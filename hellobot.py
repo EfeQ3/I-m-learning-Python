632430d932200231bdfa62d7d755444df1b4b9e8
EfeQ3 <24480946@stu.omu.edu.tr>
2024-11-22 00:38:21 +0300

    Add files via upload

---
+++ hellobot.py
@@ -0,0 +1,61 @@
+# use "#" for comment line 
+
+#declare veriable
+
+num1 = 22
+num2 = 34
+
+sum = num1+num2
+
+
+print("Sum is : ", sum) 
+#----------------------------------------------
+
+site_name = 'eftalks.com'
+
+print(site_name)
+
+##-----------------------------------------------
+
+a ,b,c = 12,43.32,'eftalks.com'
+
+print(a)
+print(b)
+print(c)
+
+#-------------------------------------------
+#data tipi yazdirma
+inte_num = 14
+float_num = 1.67
+
+new_num = inte_num+float_num
+
+print("Value : ", new_num)
+print("Data Type : ",type(new_num))
+#--------------------------------------------------------
+#data tipini değiştirme 
+num_str = "133"
+num_int = 13
+
+print("Data Type Before Converting : ",type(num_str))
+
+num_str = int(num_str)
+
+print("Data Type After Converting : ",type(num_str))
+
+num_sum = num_str+num_int
+
+print("Value is : ",num_sum)
+
+print("Value Type is : ",type(num_sum))
+
+#toplama makinesi 
+num1 = int(input("Enter a number : " ))
+num2 = int(input("Enter one more number : "))
+num_sum = num1**num2
+print("Value is : ",num_sum)
+print("Value Type is : ",type(num_sum))
+
+
+
+
